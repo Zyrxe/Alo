c9c83f679eeb0e8777097ea004844adcee6f2b57
Sel <sofia.sry7@gmail.com>
2025-10-27 08:46:45 +0700

    Update Hardhat configuration for Sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: process.env.NODE_URL_SEPOLIA || "",
-      accounts: process.env.PRIVATE_KEY_DEPLOYER ? [process.env.PRIVATE_KEY_DEPLOYER] : []
-    }
-  }
+      url: process.env.NODE_URL_SEPOLIA || "https://sepolia.drpc.org",
+      accounts: process.env.PRIVATE_KEY_DEPLOYER
+        ? [process.env.PRIVATE_KEY_DEPLOYER]
+        : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
